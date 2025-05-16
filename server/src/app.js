17a4445701873bc282476209ce4597b5e19350a2
Himani Sharma <hinusharma18@gmail.com>
2025-05-16 23:51:42 +0530

    Settings Updates

--- server/src/app.js
+++ server/src/app.js
@@ -24,15 +24,15 @@ const collaborationRoutes = require('./routes/collaborationRoutes');
 // Initialize express app
 const app = express();
 
-// Connect to database
-connectDB();
+// // Connect to database
+// connectDB();
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors({
-    origin: ['https://projectshelfs.vercel.app', 'http://localhost:5173', '*'],
+    origin: ['https://project-shelf-hs.vercel.app', 'http://localhost:5173', '*'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const { connectDB } = require('./config/database');
 
 // Load environment variables
 dotenv.config();
@@ -8,4 +9,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    // Connect to database
+    connectDB();
 });
